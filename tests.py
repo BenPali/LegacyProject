944dbf98c0adeb32e0c1d6ad37f02d90efb51206
Ben Pali <bpali2000@gmail.com>
2025-09-05 10:40:16 +0200

    trigger CI

--- tests.py
+++ tests.py
@@ -3,3 +3,9 @@ from main import hello
 
 def test_hello():
     assert hello() == "Hello World!"
+
+def test_main_execution(capsys):
+    from main import hello
+    print(hello())
+    captured = capsys.readouterr()
+    assert captured.out == "Hello World!\n"
